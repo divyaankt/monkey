095efb7d0e8094922301b1349c1578878a834ca3
Divyaank <divyaankt21@gmail.com>
2020-07-18 14:54:37 +0530

    Registered parseIntegerLiteral() in Parser struct

--- parser/parser.go
+++ parser/parser.go
@@ -48,6 +48,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
