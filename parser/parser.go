c5375e6543bd9b714cf0c951d3c3d254d68c6009
Divyaank <divyaankt21@gmail.com>
2020-07-18 14:29:26 +0530

    V1 of parseExpression()

--- parser/parser.go
+++ parser/parser.go
@@ -105,6 +105,17 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+	prefix := p.prefixParseFns[p.curToken.Type]
+
+	if prefix == nil {
+		return nil
+	}
+
+	leftExp := prefix()
+	return leftExp
+}
+
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 
