df292ac1bb8b3113d3efad6b338ee43245b1e252
Divyaank <divyaankt21@gmail.com>
2020-07-18 19:31:00 +0530

    Added precedence table

--- parser/parser.go
+++ parser/parser.go
@@ -226,3 +226,14 @@ type (
 	prefixParseFn func() ast.Expression
 	infixParseFn  func(ast.Expression) ast.Expression
 )
+
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NOT_EQ:   EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
