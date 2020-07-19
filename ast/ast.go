c65b39ad6d72f1d10c95ca61703e47be6e359fb1
Divyaank <divyaankt21@gmail.com>
2020-07-19 12:39:49 +0530

    Added BlockStatement struct and its methods in AST

--- ast/ast.go
+++ ast/ast.go
@@ -196,3 +196,18 @@ func (ie *IfExpression) String() string {
 	}
 	return out.String()
 }
+
+type BlockStatement struct {
+	Token      token.Token // the { token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode()       {}
+func (bs *BlockStatement) TokenLiteral() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
