9005bdd9234d66c855e2838c0c8b6ec8da1fddfe
Divyaank <divyaankt21@gmail.com>
2020-10-03 00:01:00 +0530

    Eval() now supports if-else expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -45,11 +45,41 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 
 	return nil
 }
 
+//Note: NULL and FALSE are not Truthy
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
 func nativeBoolToBooleanObject(input bool) *object.Boolean {
 	if input {
 		return TRUE
