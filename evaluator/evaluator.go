d49104954c28d6b249c2169ebc696b465326e386
Divyaank <divyaankt21@gmail.com>
2020-10-02 15:41:00 +0530

    Modified Eval() to parse the entire AST recursively

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -7,9 +7,29 @@ import (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+
+	//Recurisvely evaluate each Statement of the Program
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	//Evaluate an Expression Statement
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	//Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	}
 
 	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
 }
