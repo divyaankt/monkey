e4ae18166d4e316fc4633e7f363ad52cdb68d342
Divyaank <divyaankt21@gmail.com>
2020-10-02 22:17:35 +0530

    Eval() works correctly for Bang Operator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -61,3 +61,25 @@ func evalStatements(stmts []ast.Statement) object.Object {
 
 	return result
 }
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
