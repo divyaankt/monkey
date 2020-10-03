f04236adbe07b403cbd2024bd9b6f68a295fecc9
Divyaank <divyaankt21@gmail.com>
2020-10-03 23:08:51 +0530

    Added new function to evaluate the expressions passed as an argument to functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -102,9 +102,28 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if isError(function) {
 			return function
 		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
 	}
 
 	return nil
+}
+
+func evalExpressions(
+	exps []ast.Expression,
+	env *object.Environment,
+) []object.Object {
+	var result []object.Object
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
 }
 
 func evalIdentifier(
