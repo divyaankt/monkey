81405808ac3b072f12393bd1ee35bdd8ef54638f
Divyaank <divyaankt21@gmail.com>
2020-10-03 23:03:30 +0530

    Added switch case for function call expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -96,6 +96,12 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		params := node.Parameters
 		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
+
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
 	}
 
 	return nil
