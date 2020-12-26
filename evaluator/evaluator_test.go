a0d9e3fd752c241086289d0c61a9b1e444086a20
Divyaank <divyaankt21@gmail.com>
2020-12-26 20:32:04 +0530

    Added test for String concatenation operation in evaluator_test

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -52,6 +52,19 @@ func TestStringLiteral(t *testing.T) {
 	}
 }
 
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -219,6 +232,9 @@ func TestErrorHandling(t *testing.T) {
 		{
 			"5; true + false; 5",
 			"unknown operator: BOOLEAN + BOOLEAN",
+		},
+		{
+			`"Hello" - "World"`, "unknown operator: STRING - STRING",
 		},
 		{
 			"if (10 > 1) { true + false; }",
