80e5352db66742a34e6bb69c864266d0193c73ad
Divyaank <divyaankt21@gmail.com>
2020-12-27 16:36:54 +0530

    Modified applyFunction() in evaluator for handling builtin functions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -116,15 +116,20 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-
-	if !ok {
+	
+	switch fn := fn.(type) {
+	
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	
+	case *object.Builtin:
+		return fn.Fn(args...)
+	
+	default:
 		return newError("not a Function: %s", fn.Type())
 	}
-
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(
