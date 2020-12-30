de2497bc199683447aa6fea3b9ead353b00b940b
Divyaank <divyaankt21@gmail.com>
2020-12-30 18:17:21 +0530

    Array Index Operation implemented successfully

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -117,6 +117,23 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return args[0]
 		}
 		return applyFunction(function, args)
+
+	case *ast.IndexExpression:
+		//First evaluate the left operand of Array Index operation
+		left := Eval(node.Left, env)
+
+		if isError(left) {
+			return left
+		}
+
+		//Then evaluate the right operand of Array Index operation
+		index := Eval(node.Index, env)
+
+		if isError(index) {
+			return index
+		}
+
+		return evalIndexExpression(left, index)
 	}
 
 	return nil
@@ -172,6 +189,27 @@ func evalExpressions(
 		result = append(result, evaluated)
 	}
 	return result
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
 }
 
 func evalIdentifier(
