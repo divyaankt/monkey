461ce2fee695628aeae695119f982f554681cf86
Divyaank <divyaankt21@gmail.com>
2021-01-03 15:06:29 +0530

    Added comments for better understanding of Modify() in modify.go

--- ast/modify.go
+++ ast/modify.go
@@ -3,6 +3,11 @@ package ast
 type ModifierFunc func(Node) Node
 
 func Modify(node Node, modifier ModifierFunc) Node {
+	//Recursively walk down the children of any given ast.Node
+	//here certain ast.Nodes do not and won’t have their own case branch, e.g. *ast.IntegerLiteral
+	//That’s because we can’t traverse their children, even if we wanted to, because they don’t have any
+	//if they have children, as is the case with *ast.Program, we call ast.Modify with each child,
+	//which again could result in calls to ast.Modify with the children of the child, and so on
 	switch node := node.(type) {
 
 	case *Program:
