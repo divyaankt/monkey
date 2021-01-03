d4df871d9f8ca7c12c7a0823bef231f3d33dac23
Divyaank <divyaankt21@gmail.com>
2021-01-03 17:00:33 +0530

    Added case for ReturnStatements in Modify()

--- ast/modify.go
+++ ast/modify.go
@@ -42,6 +42,8 @@ func Modify(node Node, modifier ModifierFunc) Node {
 			node.Statements[i], _ = Modify(node.Statements[i], modifier).(Statement)
 		}
 
+	case *ReturnStatement:
+		node.ReturnValue, _ = Modify(node.ReturnValue, modifier).(Expression)
 	}
 
 	return modifier(node)
